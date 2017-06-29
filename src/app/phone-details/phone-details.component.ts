d46e2dc40e633c6ad8e9c75a4d3fd637b1da2586
Alice Guan <aliguan726@gmail.com>
2017-06-29 19:23:25 -0400

    First commit

---
+++ src/app/phone-details/phone-details.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { PhoneService } from '../phone.service';
+
+@Component({
+  selector: 'app-phone-details',
+  templateUrl: './phone-details.component.html',
+  styleUrls: ['./phone-details.component.css']
+})
+export class PhoneDetailsComponent implements OnInit {
+
+  phone: Object;
+
+  errorMessage: String = '';
+
+  constructor( private myRoute: ActivatedRoute,
+                private myPhoneService: PhoneService,
+                private myNavigator: Router) { }
+
+  ngOnInit() {
+    this.myRoute.params.subscribe( (params) => {
+      this.getPhoneDetails(params['id']);
+
+    });
+  }
+
+  getPhoneDetails(id) {
+    this.myPhoneService.get( id )
+    .then( (thePhoneDetails) => {
+      this.phone = thePhoneDetails;
+    })
+    .catch( (err) => {
+      this.errorMessage = 'Could not retrieve phone details. Try Again later.';
+    });
+  }
+
+  deletePhone() {
+    if (!window.confirm('Are you sure?') ) {
+      return;
+    }
+  }
+
+}
